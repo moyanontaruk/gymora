bcbac4b3806011352bd9cf0168146b79963385f3
Maggie O <3162543o@student.gla.ac.uk>
2026-07-16 17:03:50 +0100

    updating equipment class error

--- backend/app/models/equipment.py
+++ backend/app/models/equipment.py
@@ -3,10 +3,9 @@ from sqlalchemy import Text, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 class Equipment(Base):
-    __table__ = "equipment"
+    __tablename__ = "equipment"
 
     equipment_id: Mapped[int] = mapped_column(primary_key= True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     description: Mapped[str | None] = mapped_column(Text, nullable= True)
 
-    
