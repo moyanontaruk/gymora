d72e87323dbf8c1c45b66f34fe1c850b309653cf
Maggie O <3162543o@student.gla.ac.uk>
2026-07-20 21:29:03 +0100

    update with FK error

--- backend/app/models/exercise_muscle_group.py
+++ backend/app/models/exercise_muscle_group.py
@@ -7,6 +7,6 @@ class ExerciseMuscleGroup(Base):
     __tablename__ = "exercise_muscle_group"
 
     exercise_muscle_group_id: Mapped[int] = mapped_column(primary_key=True)
-    exercise_id: Mapped[int] = mapped_column(ForeignKey("exercises.exercise_id", nullable = False))
-    muscle_group_id: Mapped[int] = mapped_column(ForeignKey("muscle_groups.muscle_group_id", nullable = False))
+    exercise_id: Mapped[int] = mapped_column(ForeignKey("exercises.exercise_id"), nullable = False)
+    muscle_group_id: Mapped[int] = mapped_column(ForeignKey("muscle_groups.muscle_group_id"), nullable = False)
     is_primary: Mapped[bool] = mapped_column(Boolean, nullable= False, default = False)
