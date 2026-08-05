fd2a771629fa378ff5a9ed9b9038757bf333d888
Maggie O <3162543o@student.gla.ac.uk>
2026-08-05 16:43:50 +0100

    db stuff

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from app.routers import equipment, exercise,auth, workout_log, muscle_group
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Gymora API")
+
+app.add_middleware(
+    CORSMiddleware,
+
+    #not doing ["*"] b/c then all origins(any webssite) would be able to access
+        #listing exactly which so only the react dev server can enter
+    allow_origins=["http://localhost:5173","http://127.0.0.1:5173"],
+
+    #let browser send Authorization header with the JWT
+    allow_credentials=True,
+
+    #GET, POST, PUT, DELETE
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(equipment.router)
 app.include_router(exercise.router)
