fc313e342db5200c1d8e909d5bf1a12220585bfd
Maggie O <3162543o@student.gla.ac.uk>
2026-08-24 14:25:03 +0100

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -115,7 +115,7 @@ function Login() {
                 </form>
 
                 <p className="auth-footer">
-                    Don't have an account?
+                    Don't have an account? 
                         <Link to="/register">
                          Create one
                         </Link>
